a8bbc2b37fe9c224224d48c3c0c64d1905ae1964
Tim Brunson <tjbrunson@gmail.com>
2020-07-08 10:37:52 -0700

    minor change

--- src/main/java/org/reactome/idg/pairwise/web/errors/InternalServerError.java
+++ src/main/java/org/reactome/idg/pairwise/web/errors/InternalServerError.java
@@ -1,5 +1,19 @@
 package org.reactome.idg.pairwise.web.errors;
 
-public class InternalServerError {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 
+ * @author brunsont
+ *
+ */
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerError extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 316926435659593174L;
 
 }
