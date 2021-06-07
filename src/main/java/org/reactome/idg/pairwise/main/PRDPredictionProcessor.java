d7e8bed62cf8d8f339fb5e92531b8402859ff6a7
Tim Brunson <tjbrunson@gmail.com>
2021-06-07 14:48:37 -0700

    updated code to collect correct column and check more thoroughly against feature file

--- src/main/java/org/reactome/idg/pairwise/main/PRDPredictionProcessor.java
+++ src/main/java/org/reactome/idg/pairwise/main/PRDPredictionProcessor.java
@@ -1,14 +1,15 @@
 package org.reactome.idg.pairwise.main;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
 import org.reactome.idg.pairwise.model.GeneCombinedScore;
 import org.reactome.idg.pairwise.service.PairwiseService;
 import org.slf4j.Logger;
@@ -17,23 +18,43 @@ import org.slf4j.LoggerFactory;
 public class PRDPredictionProcessor {
     private final static Logger logger = LoggerFactory.getLogger(PathwayProcessor.class);
 
-
-	public void processPRDPredictions(PairwiseService service, String folder, String prdFile, String predictionFile) {
+    /**
+     * prdPrediction gene pairs is checked for consistency agains the feature file. After checking, 
+     * a map is build of gene to Gene to combined score maps. After the mapping is complete, insert into database through
+     * service method
+     * @param service
+     * @param folder
+     * @param prdPredictionFile
+     *     first column should be gene pair separated by underscore. Score in column 5 (index 4) will be cached as the prd for 
+     *     each gene pair
+     * @param featureFile
+     *     Only concerned with first column which must be gene pairs separated by \t.
+     */
+	public void processPRDPredictions(PairwiseService service, String folder, String prdPredictionFile, String featureFile) {
 		logger.info("Clearing combined scores...");
-//		service.clearCombinedScores(); //helper method for testing adding combined scores
+		service.clearCombinedScores(); //helper method for testing adding combined scores
 		
 		logger.info("Caching combined scores...");
 		
-		Map<String, String> underscoreToTabMap = loadUnderscoreToTabMap(folder, predictionFile);
+		Map<String, String> underscoreToTabMap = loadUnderscoreToTabMap(folder, featureFile);
 				
 		Map<String, GeneCombinedScore> geneToCombinedScore = new HashMap<>();
 		
 		try {
-			Reader in = new FileReader(folder + prdFile);
-			Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
-			for(CSVRecord record : records) {
-				String [] genes = underscoreToTabMap.get(record.get(0)).split("\t");
-				Double prd = Double.parseDouble(record.get(1));
+			BufferedReader reader = new BufferedReader(new FileReader(folder+prdPredictionFile));
+			String line = reader.readLine();
+			while((line = reader.readLine()) != null) {
+				String[] tokens = line.split(",");
+				
+				//check against underscore map to ensure features file matches prd gene pairs
+				String genesString = underscoreToTabMap.get(tokens[0]);
+				if(genesString == null) {
+					throw new IOException("Gene pair in prediction file not found in feature file: " + tokens[0]);					
+				};
+				
+				String [] genes = genesString.split("\t");
+				if(genes[0] == "null" || genes[1] == "null") continue; //make sure no nulls get cached
+				Double prd = Double.parseDouble(tokens[4]);
 
 				if(!geneToCombinedScore.containsKey(genes[0]))
 					geneToCombinedScore.put(genes[0], new GeneCombinedScore(genes[0]));
@@ -43,7 +64,7 @@ public class PRDPredictionProcessor {
 					geneToCombinedScore.put(genes[1], new GeneCombinedScore(genes[1]));
 				geneToCombinedScore.get(genes[1]).addInteractorToScore(genes[0], prd);
 			}
-			
+			reader.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			logger.error(e.getMessage());
@@ -53,10 +74,8 @@ public class PRDPredictionProcessor {
 			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
-		
-		logger.info("Finished caching " + geneToCombinedScore.keySet().size() + " genes.");
-		
-		logger.info("Ensuring all genes are in GENE_INDEX");
+				
+//		logger.info("Ensuring all genes are in GENE_INDEX");
 		service.ensureGeneIndex(geneToCombinedScore.keySet());
 		
 		logger.info("Inserting combined_scores for each relationship document");
@@ -67,12 +86,13 @@ public class PRDPredictionProcessor {
 	private Map<String, String> loadUnderscoreToTabMap(String folder, String file){
 		Map<String, String> rtn = new HashMap<>();
 		try {
-			Reader in = new FileReader(folder+file);
-			Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
-			for(CSVRecord record : records) {
-				String tabString = record.get(0);
+			BufferedReader reader = new BufferedReader(new FileReader(folder+file));
+			String line = reader.readLine(); //skip header line;
+			while((line = reader.readLine()) != null) {
+				String tabString = line.split(",")[0];
 				rtn.put(tabString.replace("\t", "_"), tabString);
 			}
+			reader.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			logger.error(e.getMessage());
