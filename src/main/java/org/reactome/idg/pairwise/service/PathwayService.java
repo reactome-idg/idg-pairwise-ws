0079f0f9ca34ce0c3a17c5df8b41631f7624a49a
Tim Brunson <tjbrunson@gmail.com>
2021-06-24 12:01:27 -0700

    removed unused import

--- src/main/java/org/reactome/idg/pairwise/service/PathwayService.java
+++ src/main/java/org/reactome/idg/pairwise/service/PathwayService.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
