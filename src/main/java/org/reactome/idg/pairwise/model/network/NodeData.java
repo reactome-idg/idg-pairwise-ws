2ddc802b9a510916a83c5403fe141a7e40a2782c
Tim Brunson <tjbrunson@gmail.com>
2021-07-14 13:43:45 -0700

    classes to return network to the front end

---
+++ src/main/java/org/reactome/idg/pairwise/model/network/NodeData.java
@@ -0,0 +1,64 @@
+package org.reactome.idg.pairwise.model.network;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+/**
+ * 
+ * @author brunsont
+ *
+ */
+@JsonInclude(Include.NON_NULL)
+public class NodeData extends Data {
+
+	private Double weightedTDL;
+	private Double fdr;
+	private Double pVal;
+	private String weightedTDLColorHex;
+	
+	public NodeData() {/*Nothing Here*/}
+	
+	
+
+	public NodeData(String id, String name, Double weightedTDL, Double fdr, Double pVal, String weightedTDLColorHex) {
+		super(id, name);
+		this.weightedTDL = weightedTDL;
+		this.fdr = fdr;
+		this.pVal = pVal;
+		this.weightedTDLColorHex = weightedTDLColorHex;
+	}
+
+
+
+	public Double getWeightedTDL() {
+		return weightedTDL;
+	}
+
+	public void setWeightedTDL(Double weightedTDL) {
+		this.weightedTDL = weightedTDL;
+	}
+
+	public Double getFdr() {
+		return fdr;
+	}
+
+	public void setFdr(Double fdr) {
+		this.fdr = fdr;
+	}
+
+	public Double getpVal() {
+		return pVal;
+	}
+
+	public void setpVal(Double pVal) {
+		this.pVal = pVal;
+	}
+
+	public String getWeightedTDLColorHex() {
+		return weightedTDLColorHex;
+	}
+
+	public void setWeightedTDLColorHex(String weightedTDLColorHex) {
+		this.weightedTDLColorHex = weightedTDLColorHex;
+	}
+}
