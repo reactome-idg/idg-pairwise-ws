c49a65d83cf2578af230131c4727552d9e4ccb02
Tim Brunson <tjbrunson@gmail.com>
2021-07-14 13:45:51 -0700

    inserts pathway data into mongodb

--- src/main/java/org/reactome/idg/pairwise/main/GeneToPathwayProcessor.java
+++ src/main/java/org/reactome/idg/pairwise/main/GeneToPathwayProcessor.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,6 +17,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.math.distribution.HypergeometricDistributionImpl;
+import org.reactome.idg.pairwise.model.Pathway;
+import org.reactome.idg.pairwise.model.PathwayOverlap;
 import org.reactome.idg.pairwise.model.pathway.gene.ProteinTargetDevLevel;
 import org.reactome.idg.pairwise.service.PairwiseService;
 import org.slf4j.Logger;
@@ -43,10 +47,8 @@ public class GeneToPathwayProcessor {
     	service.regenerateReactomePathwaysCacheCollection();
     	logger.info("Processing gene to Pathways");
 		Map<String, String> uniprotToGene = service.getUniProtToGene();
-    	Map<String, Set<String>> pathwayToGeneSet = new HashMap<>();
-    	Map<String, Double> pathwayToWeightedTDL = new HashMap<>();
-    	
-    	//build map of pathay stId to set of genes in pathway
+    	Map<String, Pathway> pathwayStIdToPathway = new HashMap<>();    	
+    	//build map of pathway stId to Pathway object
     	try {
     		URL url = new URL(UNIPROT_2_REACTOME_ALL_LEVELS_URL);
     		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -58,35 +60,66 @@ public class GeneToPathwayProcessor {
     			if(geneName == null) continue;
     			String pw = tokens[1];
     			
-    			if(!pathwayToGeneSet.containsKey(pw)) pathwayToGeneSet.put(pw, new HashSet<>());
-    			pathwayToGeneSet.get(pw).add(geneName);
+    			if(!pathwayStIdToPathway.containsKey(pw)) pathwayStIdToPathway.put(pw, new Pathway(pw));
+    			pathwayStIdToPathway.get(pw).addGene(geneName);
     		}
     		br.close();
     	} catch(IOException e) {
     		logger.error(e.getMessage(), e);
     		return;
     	}
-    	
     	//build map of pathway to weighted target development level score
     	logger.info("Processing weighted average target development levels");
-    	Map<String, String> genes = getGeneTargetDevLevels();
+    	Map<String, String> geneToTDL = getGeneTargetDevLevels();
+    	List<String> pathwayStIds = new ArrayList<>(pathwayStIdToPathway.keySet());
+    	int totalGenesInReactome = service.getTotalReactomeGenes();
     	
-    	pathwayToGeneSet.forEach((pw, geneList) ->{
-    		//list of TDLs for pw based on the pw's geneList
-    		Collection<String> tdls = geneList.stream().filter(genes::containsKey).collect(Collectors.toMap(Function.identity(), genes::get)).values();
-    		//calculate weighted average
-    		double tdark = Collections.frequency(tdls, "Tdark")*1;
-    		double tbio = Collections.frequency(tdls, "Tbio")*2;
-    		double tchem = Collections.frequency(tdls, "Tchem")*3;
-    		double tclin = Collections.frequency(tdls, "Tclin")*4;
-    		double avg = (tdark+tbio+tclin+tchem)/tdls.size();
-    		//put weighted TDL on map for inserting into database
-    		pathwayToWeightedTDL.put(pw, avg);
-    	});
+    	for(int i = 0; i<pathwayStIds.size()-1; i++) {
+    		
+    		Pathway from = pathwayStIdToPathway.get(pathwayStIds.get(i));
+    		if(from.getGenes() == null || from.getGenes().size() == 0) continue;
+    		from.setWeightedTDL(getWeightedTDLScore(geneToTDL, from.getGenes()));
+    		
+    		for(int j = i+1; j<pathwayStIds.size(); j++) {
+    			
+    			Pathway to = pathwayStIdToPathway.get(pathwayStIds.get(j));
+    			if(to == null || to.getGenes().size() == 0) continue;
+    			
+    			//get common genes between two pathways
+    			Set<String> commonGenes = new HashSet<>(from.getGenes());
+    			commonGenes.retainAll(to.getGenes());
+    			
+    			
+      			HypergeometricDistributionImpl hyper = new HypergeometricDistributionImpl(totalGenesInReactome,
+      																					  from.getGenes().size(),
+      																					  to.getGenes().size());
+      			
+      			Double p = hyper.upperCumulativeProbability(commonGenes.size());
+      			
+      			from.addStIdToHypergeometricScoreRelationship(to.getStId(), new PathwayOverlap(to.getStId(), p, commonGenes.size()));
+      			to.addStIdToHypergeometricScoreRelationship(from.getStId(), new PathwayOverlap(from.getStId(), p, commonGenes.size()));
+    		}
+    	}
+    	
+    	//add weightedTDL for last pathway
+    	Pathway lastPW = pathwayStIdToPathway.get(pathwayStIds.get(pathwayStIds.size() - 1));
+    	lastPW.setWeightedTDL(getWeightedTDLScore(geneToTDL, lastPW.getGenes()));
     	
     	logger.info("Inserting pathways into database");
-    	service.insertReactomePathwayCache(pathwayToGeneSet, pathwayToWeightedTDL);
+    	service.insertReactomePathwayCache(pathwayStIdToPathway.values());
     	logger.info("Done inserting pathways into database");
+    }
+    
+    private Double getWeightedTDLScore(Map<String, String> geneToTDL, List<String> genes) {
+    	Collection<String> tdls = genes.stream().filter(geneToTDL::containsKey)
+				   .collect(Collectors.toMap(Function.identity(), geneToTDL::get))
+				   .values();
+		//calculate weighted average
+		double tdark = Collections.frequency(tdls, "Tdark")*1;
+		double tbio = Collections.frequency(tdls, "Tbio")*2;
+		double tchem = Collections.frequency(tdls, "Tchem")*3;
+		double tclin = Collections.frequency(tdls, "Tclin")*4;
+		return (tdark+tbio+tclin+tchem)/tdls.size();
     }
 
 	private Map<String, String> getGeneTargetDevLevels() {
